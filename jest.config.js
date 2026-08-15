40f9091a6b026492a6948acd440cf4533cb7d6b9
Sagar <sainsagar96@gmail.com>
2026-08-15 17:37:06 +0530

    improved template configurations

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 /* eslint-disable no-undef */
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  verbose: true,
+  collectCoverage: true,
+  coverageProvider: "v8",
+  collectCoverageFrom: ["src/**/*.ts", "!tests/**", "!**/node_modules/**"],
 };
